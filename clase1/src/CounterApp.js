fc16be1ec7b80c91584b8eb9cb4860637e38c898
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-12-01 00:01:59 -0400

    pull de origin

--- clase1/src/CounterApp.js
+++ clase1/src/CounterApp.js
@@ -2,10 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const CounterApp = ({ value }) => {
+const saludar = (e)  => { console.log(e)}
+
     return (
         <>
             <h1>CounterApp</h1>
             <h2> {value} </h2>
+            <button onClick={saludar}>+1</button>  {/* pasamos como referencia la funcion */}
         </>
     );
 };
