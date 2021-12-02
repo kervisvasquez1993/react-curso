2d46673c6efca013f0b68dbf84ed3ee1861decab
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-12-02 16:58:34 -0400

    cremaos componente de ejemplo

--- 04-gif-expert/src/component/GifExpertApp.js
+++ 04-gif-expert/src/component/GifExpertApp.js
@@ -1,15 +1,29 @@
-import React from 'react';
-
+import React, { useState } from "react";
 
 const GifExpertApp = () => {
+    // const categories = [ 'One Punch,', 'Samurai X,', 'Dragon ball'];
 
+    const [categorias, setCategorias] = useState([
+        "One Punch,",
+        "Samurai X,",
+        "Dragon ball",
+    ]);
+
+    const handleApp = ( value ) => {
+        setCategorias([...categorias, value]);
+    };
     return (
         <>
             <h2>GitExpertApp</h2>
-            <hr/>
+            <button onClick={() => handleApp('test1')}>agregar</button>
+            <hr />
+            <ol>
+                {categorias.map((category, i) => {
+                    return <li key={i}>{category}</li>;
+                })}
+            </ol>
         </>
     );
-}
-
+};
 
 export default GifExpertApp;
