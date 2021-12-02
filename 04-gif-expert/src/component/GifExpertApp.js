74b38193bd8e20944124cace375f892a44038c08
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-12-02 15:27:01 -0400

    agregamos primer componente en react

---
+++ 04-gif-expert/src/component/GifExpertApp.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const GifExpertApp = () => {
+
+    return (
+        <>
+            <h2>GitExpertApp</h2>
+            <hr/>
+        </>
+    );
+}
+
+
+export default GifExpertApp;
