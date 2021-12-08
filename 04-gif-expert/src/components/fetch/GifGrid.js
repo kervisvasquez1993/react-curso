b14cc1b4d2728881d8fc23265d5b3a15cc65e4a3
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-12-08 15:05:06 -0400

    agregamos componente para crear imagenes

--- 04-gif-expert/src/components/fetch/GifGrid.js
+++ 04-gif-expert/src/components/fetch/GifGrid.js
@@ -1,32 +1,48 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
+import {GifGridItem} from "./GifGridItem";
 
 const GifGrid = ({ category }) => {
-    const [count, setCount] = useState(0);
+    const [images, setImages] = useState([]);
     useEffect(() => {
         getGifs();
-    }, [])
+    }, []);
     const getGifs = async () => {
         const url =
-            "https://api.giphy.com/v1/gifs/search?api_key=RCdtf91rKTM96WoidRUsU2OP1XMkVyoW&q=goku&limit=5";
+            "https://api.giphy.com/v1/gifs/search?api_key=RCdtf91rKTM96WoidRUsU2OP1XMkVyoW&q=goku&limit=10";
         const resp = await fetch(url);
-        const {data} = await resp.json(); 
-        const gifs = data.map(img => {
+        const { data } = await resp.json();
+        const gifs = data.map((img) => {
             return {
-                id : img.id,
-                title : img.title,
-                url : img.images?.downsized_medium.url
-
-            }
-        })
+                id: img.id,
+                title: img.title,
+                url: img.images?.downsized_medium.url,
+            };
+        });
         console.log(gifs);
+        setImages(gifs);
     };
 
     // getGifs(); si se deja en este nivel se vuelve e llamar
     return (
         <>
             <h3>{category}</h3>
-            <h4>{count}</h4>
-            <button onClick={()=> setCount(count + 1)}></button>
+            
+                {/* {images.map((img) => (
+                    <li key={img.id}>{img.title}</li>
+                ))} */
+                // images.map(({id, title}) => (
+                //     <li key={id}>{title}</li>
+                // ))
+
+                images.map((img) => (
+                    <GifGridItem 
+                    key={img.id}
+                    { ...img }/>
+                ))
+                
+
+                }
+            
         </>
     );
 };
