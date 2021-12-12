72a3a7f771f91db5a77ce58b17dbcc020421def0
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-12-11 21:04:16 -0400

    primera prueba

--- clase1/src/tests/demo.test.js
+++ clase1/src/tests/demo.test.js
@@ -1,1 +1,22 @@
-console.log('hola desde test')
+test('debe de ser debe de ser iguales los string', () => {
+    // const isActive = false;
+
+    // if(!isActive){
+    //     throw new Error('no esta activo')
+    // }
+
+    // inicializacion
+    const mensaje = 'hola mundo';
+
+    // estimulo 
+
+    const mesnaje2 = `hola mundo!`
+
+    //observar el comportamiento
+
+
+    expect(mensaje).toBe(mesnaje2);
+
+
+
+})
