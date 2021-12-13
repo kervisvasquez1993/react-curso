002b5f0eb0a4dfee16ecb48a2d2e3a42d3a477db
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-12-13 16:50:39 -0400

    validamos campos que todos esten llenos

--- proyecto-juan-proyecto-1/cita-react/src/components/Formulario.jsx
+++ proyecto-juan-proyecto-1/cita-react/src/components/Formulario.jsx
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useState, useEfect } from "react";
+import { Mensaje } from "./Mensaje";
 
 export const Formulario = () => {
+    const [nombre, setNombre] = useState("");
+    const [propietario, setPropietario] = useState("");
+    const [email, setEmail] = useState("");
+    const [fecha, setFecha] = useState("");
+    const [sintomas, setSintomas] = useState("");
+    const [error, setError] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // validacion del formulario
+        if (
+            [nombre, propietario, email, fecha, setFecha, sintomas].includes("")
+        ) {
+            console.log("campo vacio ");
+            setError(true);
+            return;
+        }
+        setError(false);
+    };
+
     return (
         <div className="md:w-1/2 lg:w-2/5">
             <h2 className="font-black text-3xl text-center">
@@ -14,19 +35,27 @@ export const Formulario = () => {
                 </span>
             </p>
 
-            <form className="bg-white my-5 shadow-md rounded-lg py-10 px-5">
+            <form
+                onSubmit={handleSubmit}
+                className="bg-white my-5 shadow-md rounded-lg py-10 px-5"
+            >
+                {error && <Mensaje />}
                 <div className="mb-5">
                     <label
                         className="block  text-grey-700 uppercase font-bold"
                         htmlFor="mascota"
                     >
-                        NOMBRE
+                        NOMBRE : {nombre}
                     </label>
                     <input
                         id="mascota"
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
                         type="text"
                         placeholder="nombre mascota"
+                        value={nombre}
+                        onChange={(e) => {
+                            setNombre(e.target.value);
+                        }}
                     />
                 </div>
                 <div className="mb-5">
@@ -34,13 +63,17 @@ export const Formulario = () => {
                         className="block  text-grey-700 uppercase font-bold"
                         htmlFor="propietario"
                     >
-                        PROPIETARIO
+                        PROPIETARIO : {propietario}
                     </label>
                     <input
                         id="propietario"
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
                         type="text"
                         placeholder="Propietario"
+                        value={propietario}
+                        onChange={(e) => {
+                            setPropietario(e.target.value);
+                        }}
                     />
                 </div>
                 <div className="mb-5">
@@ -48,13 +81,17 @@ export const Formulario = () => {
                         className="block  text-grey-700 uppercase font-bold"
                         htmlFor="email"
                     >
-                        EMAIL
+                        EMAIL : {email}
                     </label>
                     <input
                         id="email"
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
                         type="email"
                         placeholder="email@ejemplo.com"
+                        value={email}
+                        onChange={(e) => {
+                            setEmail(e.target.value);
+                        }}
                     />
                 </div>
                 <div className="mb-5">
@@ -62,12 +99,16 @@ export const Formulario = () => {
                         className="block  text-grey-700 uppercase font-bold"
                         htmlFor="date"
                     >
-                        FECHA
+                        FECHA : {fecha}
                     </label>
                     <input
                         id="date"
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
                         type="date"
+                        value={fecha}
+                        onChange={(e) => {
+                            setFecha(e.target.value);
+                        }}
                     />
                 </div>
                 <div className="mb-5">
@@ -83,6 +124,10 @@ export const Formulario = () => {
                         placeholder="Describe los sintomas"
                         cols="30"
                         rows="10"
+                        value={sintomas}
+                        onChange={(e) => {
+                            setSintomas(e.target.value);
+                        }}
                     />
                 </div>
 
--- proyecto-juan-proyecto-1/cita-react/src/components/ListadoPaciente.jsx
+++ proyecto-juan-proyecto-1/cita-react/src/components/ListadoPaciente.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import React from "react";
+import { Paciente } from "./Paciente";
 export const ListadoPaciente = () => {
     return (
-        <div className="w-1/2">
-            <h3>Listado Listado Paciente</h3>
+        <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+            <h2 className="font-black text-3xl text-center">
+                Listado Listado Paciente
+            </h2>
+
+            <p className="text-xl mt-5 mb-10 text-center">
+                Administra tus{" "}
+                <span className="text-indigo-600 font-bold mt-5 mb-10 text-center">
+                    {" "}
+                    Pacientes y Citas{" "}
+                </span>
+            </p>
+            <Paciente />
         </div>
-    )
-}
+    );
+};
