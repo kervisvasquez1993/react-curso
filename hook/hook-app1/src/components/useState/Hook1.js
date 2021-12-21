534202ff3f210a2985a602bc382ac2b86bcc5f56
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-12-21 16:21:23 -0400

    agregamos forma de resolver un objeto en state

--- hook/hook-app1/src/components/useState/Hook1.js
+++ hook/hook-app1/src/components/useState/Hook1.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./counter.css";
 export const Hook1 = () => {
-    const [{ contador1, contador2 }, setContador] = useState({
-        contador1: 1,
-        contador2: 2,
+    const [state, setState] = useState({
+        counter1: 1,
+        counter2: 2,
     });
+    const { counter1, counter2 } = state;
     //  const sumar = () => {
     //  setContador(contador2 + 1);
     //  };
@@ -13,15 +14,15 @@ export const Hook1 = () => {
         <>
             {/* <h1>Counter {contador}</h1> */}
 
-            <h1>contador1 : {contador1}</h1>
-            <h1>contador2 : {contador2}</h1>
+            <h1>contador1 : {counter1}</h1>
+            <h1>contador2 : {counter2}</h1>
             <hr />
             <button
                 className="btn btn-primary"
                 onClick={() =>
-                    setContador({
-                        contador1: contador1,
-                        contador2: contador2 + 1,
+                    setState({
+                        ...state,
+                        counter1: counter1 + 1,
                     })
                 }
             >
