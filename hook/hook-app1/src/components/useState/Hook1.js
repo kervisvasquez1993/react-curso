089f455100737e974b8bf3208a1c8cb4a9e7b5b5
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-12-21 16:14:08 -0400

    state con objeto

--- hook/hook-app1/src/components/useState/Hook1.js
+++ hook/hook-app1/src/components/useState/Hook1.js
@@ -1,17 +1,30 @@
 import React, { useState, useEffect } from "react";
-import './counter.css'
+import "./counter.css";
 export const Hook1 = () => {
-    const [contador, setContador] = useState(0);
-    const sumar = () => {
-        setContador(contador + 1);
-    };
-    
+    const [{ contador1, contador2 }, setContador] = useState({
+        contador1: 1,
+        contador2: 2,
+    });
+    //  const sumar = () => {
+    //  setContador(contador2 + 1);
+    //  };
 
     return (
         <>
-            <h1>Counter {contador}</h1>
+            {/* <h1>Counter {contador}</h1> */}
+
+            <h1>contador1 : {contador1}</h1>
+            <h1>contador2 : {contador2}</h1>
             <hr />
-            <button className="btn btn-primary" onClick={() => sumar()}>
+            <button
+                className="btn btn-primary"
+                onClick={() =>
+                    setContador({
+                        contador1: contador1,
+                        contador2: contador2 + 1,
+                    })
+                }
+            >
                 +1
             </button>
         </>
