48eecf1594642c8f43d8cbdcb6cc7d0478369ee5
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-12-23 10:10:30 -0400

    agregamos cambios en nuestro formulario para useEffect

---
+++ hook/hook-app1/src/components/useEffect/SimpleForm.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import "./effect.css";
+
+export const SimpleForm = () => {
+    const [form, setForm] = useState({
+        name: "",
+        email: "",
+    });
+    const { name, email } = form;
+
+    // const { name, email } = state;
+    useEffect(() => {
+        console.log("formulario cambio");
+    }, [form]);
+
+    useEffect(() => {
+        console.log("cambio el email")
+    }, [email]);
+
+    const inputChange = ({ target }) => {
+        setForm({
+            ...form,
+
+            [target.name]: target.value,
+        });
+    };
+
+
+
+    return (
+        <>
+            <h1>useEffect</h1>
+
+            <div className="form-group my-2">
+                <input
+                    type="text"
+                    name="name"
+                    className="form-control"
+                    placeholder="nombre"
+                    autoComplete="off"
+                    value={name}
+                    onChange={inputChange}
+                />
+            </div>
+            <div className="form-group my-2">
+                <input
+                    type="text"
+                    name="email"
+                    className="form-control"
+                    placeholder="email"
+                    autoComplete="off"
+                    value={email}
+                    onChange={inputChange}
+                />
+            </div>
+        </>
+    );
+};
--- hook/hook-app1/src/index.js
+++ hook/hook-app1/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {CounterWithCustomHook} from './components/useState/CounterWithCustomHook'
-
+// import {CounterWithCustomHook} from './components/useState/CounterWithCustomHook'
+import { SimpleForm } from './components/useEffect/SimpleForm';
 ReactDOM.render(
   <React.StrictMode>
-      <CounterWithCustomHook/>
+      {/* <CounterWithCustomHook/> */}
+      <SimpleForm/>
   </React.StrictMode>,
   document.getElementById('root')
 );
