b39ecf5f41b70816b612b76afaefbf5a349c1f0a
kervisvasquez1993 <kervisvasquez24@gmail.com>
2022-01-10 09:15:21 -0400

    agregamos reset a nuestro useForm

--- hook/hook-app1/src/components/08-recude/TodoApp.js
+++ hook/hook-app1/src/components/08-recude/TodoApp.js
@@ -1,6 +1,7 @@
 import React, { useReducer } from "react";
 import { todoReduce } from "./todoReduce";
 import "./style.css";
+import { useForm } from "../../hook/useForm";
 
 export const TodoApp = () => {
     const initialState = [
@@ -12,21 +13,33 @@ export const TodoApp = () => {
     ];
 
     const [todos, dispatch] = useReducer(todoReduce, initialState);
+    const [{
+        description
+    }, handleInputChange, reset] = useForm({
+        description: ''
+    })
+
+    // console.log(description)
     // console.log(todos);
     const handleSubmit = (e) => {
         e.preventDefault()
+        if(description.trim().length <= 1){
+            console.log('hola')
+            return;
+        }
         // console.log('llenando formulario')
         const newTodo = {
             id: new Date().getTime(),
-            desc: "Nueva tarea",
+            desc: description,
             done: false,
         }
 
         const agregar = {
-            type : 'add',
-            payload : newTodo
+            type: 'add',
+            payload: newTodo
         }
         dispatch(agregar)
+        reset()
     }
     return (
         <div>
@@ -57,9 +70,11 @@ export const TodoApp = () => {
                         <input
                             type="text"
                             className="form-control"
-                            name="descripcion"
+                            name="description"
                             placeholder="Aprtender.."
                             autoComplete="off"
+                            onChange={handleInputChange}
+                            value={description}
                         />
                         <button
                             type="submit"
