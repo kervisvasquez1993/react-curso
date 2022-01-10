9a320b7db93ec9eb3672c98d402bc2c8d43a6f6f
kervisvasquez1993 <kervisvasquez24@gmail.com>
2022-01-10 08:31:29 -0400

    agregamos con reduceer

--- hook/hook-app1/src/components/08-recude/TodoApp.js
+++ hook/hook-app1/src/components/08-recude/TodoApp.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from "react";
-import todoReduce from "./todoReduce";
+import { todoReduce } from "./todoReduce";
 import "./style.css";
 
 export const TodoApp = () => {
@@ -11,19 +11,68 @@ export const TodoApp = () => {
         },
     ];
 
-    const [todos] = useReducer(todoReduce, initialState);
-    console.log(todos);
+    const [todos, dispatch] = useReducer(todoReduce, initialState);
+    // console.log(todos);
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        // console.log('llenando formulario')
+        const newTodo = {
+            id: new Date().getTime(),
+            desc: "Nueva tarea",
+            done: false,
+        }
+
+        const agregar = {
+            type : 'add',
+            payload : newTodo
+        }
+        dispatch(agregar)
+    }
     return (
         <div>
-            <h1>Todo App({todos.length})</h1>
+            <h1>Todo App ({todos.length})</h1>
             <hr />
-            <ul className="list-group list-group-flush">
-                {todos.map((todo, indeice) => {
-                    <li key={todo.id} className="list-group-item">
-                       {indeice + 1} {todo.desc}
-                    </li>;
-                })}
-            </ul>
+            <div className="row">
+                <div className="col-7">
+                    <ul className="list-group list-group-flush">
+                        {todos.map((todo, i) =>
+                        (
+                            <li key={todo.id}
+                                className="list-group-item"
+                            >
+                                <p className="text-center ">{i + 1}:{todo.desc} </p>
+
+                                <button className="btn btn-danger"> borrar</button>
+                            </li>
+
+                        ))}
+                    </ul>
+                </div>
+                <div className="col-5">
+                    <h4 className=""> Agregar TODO</h4>
+
+                    <hr />
+                    <form onSubmit={handleSubmit}>
+
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="descripcion"
+                            placeholder="Aprtender.."
+                            autoComplete="off"
+                        />
+                        <button
+                            type="submit"
+                            className="btn btn-outline-primary mt-1 btn-block">
+                            Agregar
+                        </button>
+
+                    </form>
+                </div>
+
+            </div>
+
+
         </div>
     );
 };
--- hook/hook-app1/src/components/08-recude/todoReduce.js
+++ hook/hook-app1/src/components/08-recude/todoReduce.js
@@ -1,7 +1,10 @@
-const todoReduce = (state = [], action) => {
-    switch(action.type ){
-        // case 'add' :
-        default : 
+export const todoReduce = (state = [], action) => {
+    switch (action.type) {
+        case 'add':
+
+            return [...state, action.payload]
+        default:
             return state
     }
+
 }
