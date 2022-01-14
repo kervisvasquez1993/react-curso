ee592656e6b9e6440d93f78eac197b90269baee7
kervisvasquez1993 <kervisvasquez24@gmail.com>
2022-01-14 14:09:53 -0400

    usamos NavLink y agregamos ejemplo en NacBar.js

--- hook/hook-app1/src/components/09-useContext/NavBar.js
+++ hook/hook-app1/src/components/09-useContext/NavBar.js
@@ -1,20 +1,37 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 export const NavBar = () => {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-            </ul>
+        // <nav>
+        //     <ul>
+        //         <li>
+        //             <Link to="/">Home</Link>
+        //         </li>
+        //         <li>
+        //             <Link to="/about">About</Link>
+        //         </li>
+        //         <li>
+        //             <Link to="/login">Login</Link>
+        //         </li>
+        //     </ul>
+        // </nav>
+
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link className="navbar-brand" to="/">UseContext</Link>
+
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    
+                        <NavLink  exact activeClassName="active"className="nav-item" to="/">Home</NavLink>                   
+                        <NavLink  exact activeClassName="active"className="nav-nav-item" to="/login">Login</NavLink>                    
+                        <NavLink  exact activeClassName="active"className="nav-item " to="/about">About</NavLink>
+                    
+
+                </ul>
+            </div>
         </nav>
+
+
     )
 }
