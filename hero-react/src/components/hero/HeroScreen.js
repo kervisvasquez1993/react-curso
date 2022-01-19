52a513bd5ae8e369965b5166b86eaa3fb396b3e4
kervisvasquez1993 <kervisvasquez24@gmail.com>
2022-01-19 17:32:28 -0400

    usamos useNavigate para regresarnos en la navegacion

--- hero-react/src/components/hero/HeroScreen.js
+++ hero-react/src/components/hero/HeroScreen.js
@@ -1,17 +1,46 @@
-import { useParams, Navigate } from "react-router-dom";
+import { useParams, Navigate, useNavigate } from "react-router-dom";
 import { getHeroById } from "../../helpers/getHeroById";
 
 export const HeroScreen = () => {
   const { heroId } = useParams();
-
+  const navigate = useNavigate();
   const hero = getHeroById(heroId);
 
   if (!hero) {
     return <Navigate to="/error" />;
   }
+
+  const handleReturn = () => {
+    navigate(-1);
+  };
+
+  const { alter_ego, superhero, publisher, first_appearance, characters } =
+    hero;
+  let imgPath = `/assets/img/${heroId}.jpg`;
   return (
-    <div>
-      <h1>{hero.superhero}</h1>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img alt={superhero} className="img-thumbnail" src={imgPath} />
+      </div>
+      <div className="col-8">
+        <h3>{hero.superhero}</h3>
+        <ul className="list-group ">
+          <li className="list-group-item">
+            <b>Alter ego :</b> {alter_ego}
+          </li>
+          <li className="list-group-item">
+            <b>Publisher :</b> {publisher}
+          </li>
+          <li className="list-group-item">
+            <b>first Appearance :</b> {first_appearance}
+          </li>
+        </ul>
+        <h6>Characters</h6>
+        <p>{characters}</p>
+        <button className="btn btn-primary" onClick={handleReturn}>
+          Regresar
+        </button>
+      </div>
     </div>
   );
 };
