c9712f7e1ffe8440b40c6142bef10f55495e03fc
kervisvasquez1993 <kervisvasquez24@gmail.com>
2022-01-20 15:19:07 -0400

    agreganos query string

--- hero-react/src/components/search/SearchScreen.js
+++ hero-react/src/components/search/SearchScreen.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import queryString from "query-string";
+import { getHeroByName } from "../../helpers/getHeroByName";
+import { useForm } from "../../hook/useForm";
+import { HeroCard } from "../hero/HeroCard";
 
 export const SearchScreen = () => {
-    return (
-        <div>
-            <h2>SEARCH PAGE</h2>
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { q = "" } = queryString.parse(location.search);
+  const [values, handleInputChange] = useForm({ formText: q });
+
+  const { formText } = values;
+  const heroCard = getHeroByName("hola");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate(`?q=${formText}`);
+  };
+
+  return (
+    <>
+      <h2>Buscar Herores</h2>
+      <div className="row mt-5">
+        <div className="col-5">
+          <h4 className="">Busqueda</h4>
+          <hr />
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="buscar por heroe"
+              className="form-control"
+              name="formText"
+              autoComplete="off"
+              value={formText}
+              onChange={handleInputChange}
+            />
+
+            <div className="d-grid gap-2">
+              <button className="btn btn-primary mt-3" type="submit">
+                Button
+              </button>
+            </div>
+          </form>
         </div>
-    )
-}
+        <div className="col-7">
+          <h4 className="">Busqueda</h4>
+          <hr />
+          <div>
+            {heroCard.map((hero) => (
+              <HeroCard key={hero.id} {...hero} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
---
+++ hero-react/src/helpers/getHeroByName.js
@@ -0,0 +1,7 @@
+import { heroes } from "../data/Heros";
+export const getHeroByName = (name = "") => {
+  //   name = name.toLocaleLowerCase();
+  //   return heroes.filter((hero) => hero.superhero.toLocaleLowerCase.includes(name));
+
+  return heroes;
+};
