c97b70a376ced896ebd0e2c027fafb267535edcf
kervisvasquez1993 <kervisvasquez24@gmail.com>
2022-01-31 23:43:03 -0400

    agregamos pagina de nosotros

--- proyecto-juan-proyecto-1/guitalla-next/pages/nosotros.js
+++ proyecto-juan-proyecto-1/guitalla-next/pages/nosotros.js
@@ -1,11 +1,38 @@
-import Link from "next/link";
+import Image from "next/image";
 import Layout from "../components/Layout";
+import style from "../styles/Nosotros.module.css"
 
 const Nosotros = () => {
   return (
     <Layout pagina="Nosotros">
-      <h1>Desde nosotros</h1>
-      <Link href="/">ir a inicio</Link>
+      <main className="contenedor">
+        <h2 className="heading">Nosotros</h2>
+        <div  className={style.contenido}>
+          <Image 
+         layout="responsive" width={600} height={450} src="/img/nosotros.jpg" alt="nosotros" />
+          <div>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+            </p>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </p>
+          </div>
+        </div>
+      </main>
     </Layout>
   );
 };
